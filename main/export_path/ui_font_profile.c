e05013d7bd65605e2c0186664ade43e447230c84
Hokori23 <hokori23@qq.com>
2026-07-01 02:12:10 +0800

    fix: 'C文案改成°C

--- main/export_path/ui_font_profile.c
+++ main/export_path/ui_font_profile.c
@@ -2,9 +2,33 @@
 
 #include "ui.h"
 
-static void ui_font_set_fallback(const lv_font_t *font, const lv_font_t *fallback)
+static lv_font_t s_typoder_16;
+static lv_font_t s_typoder_20;
+static lv_font_t s_typoder_24;
+static lv_font_t s_typoder_28;
+static lv_font_t s_typoder_32;
+static lv_font_t s_typoder_36;
+static lv_font_t s_typoder_44;
+static lv_font_t s_typoder_56;
+static lv_font_t s_taikong_32;
+static lv_font_t s_taikong_40;
+static lv_font_t s_taikong_48;
+static lv_font_t s_taikong_56;
+static lv_font_t s_taikong_64;
+static lv_font_t s_taikong_72;
+static lv_font_t s_taikong_108;
+static lv_font_t s_taikong_128;
+static lv_font_t s_baby_gear_48;
+static lv_font_t s_baby_56;
+static lv_font_t s_baby_108;
+static lv_font_t s_baby_156;
+static lv_font_t s_baby_180;
+static lv_font_t s_baby_200;
+
+static void ui_font_set_fallback(lv_font_t *dst, const lv_font_t *src, const lv_font_t *fallback)
 {
-    ((lv_font_t *)font)->fallback = fallback;
+    *dst = *src;
+    dst->fallback = fallback;
 }
 
 void ui_font_profile_init(void)
@@ -15,31 +39,30 @@ void ui_font_profile_init(void)
     }
     initialized = true;
 
-    ui_font_set_fallback(&ui_font_FontTypoderSize16, &lv_font_montserrat_16);
-    ui_font_set_fallback(&ui_font_FontTypoderSize20, &lv_font_montserrat_16);
-    ui_font_set_fallback(&ui_font_FontTypoderSize24, &lv_font_montserrat_26);
-    ui_font_set_fallback(&ui_font_FontTypoderSize28, &lv_font_montserrat_26);
-    ui_font_set_fallback(&ui_font_FontTypoderSize32, &lv_font_montserrat_32);
-    ui_font_set_fallback(&ui_font_FontTypoderSize36, &lv_font_montserrat_32);
-    ui_font_set_fallback(&ui_font_FontTypoderSize40, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTypoderSize44, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTypoderSize56, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_typoder_16, &ui_font_FontTypoderSize16, &lv_font_montserrat_16);
+    ui_font_set_fallback(&s_typoder_20, &ui_font_FontTypoderSize20, &lv_font_montserrat_16);
+    ui_font_set_fallback(&s_typoder_24, &ui_font_FontTypoderSize24, &lv_font_montserrat_26);
+    ui_font_set_fallback(&s_typoder_28, &ui_font_FontTypoderSize28, &lv_font_montserrat_26);
+    ui_font_set_fallback(&s_typoder_32, &ui_font_FontTypoderSize32, &lv_font_montserrat_32);
+    ui_font_set_fallback(&s_typoder_36, &ui_font_FontTypoderSize36, &lv_font_montserrat_32);
+    ui_font_set_fallback(&s_typoder_44, &ui_font_FontTypoderSize44, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_typoder_56, &ui_font_FontTypoderSize56, &lv_font_montserrat_48);
 
-    ui_font_set_fallback(&ui_font_FontTaikongSize32, &lv_font_montserrat_32);
-    ui_font_set_fallback(&ui_font_FontTaikongSize40, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTaikongSize48, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTaikongSize56, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTaikongSize64, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTaikongSize72, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTaikongSize108, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontTaikongSize128, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_32, &ui_font_FontTaikongSize32, &lv_font_montserrat_32);
+    ui_font_set_fallback(&s_taikong_40, &ui_font_FontTaikongSize40, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_48, &ui_font_FontTaikongSize48, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_56, &ui_font_FontTaikongSize56, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_64, &ui_font_FontTaikongSize64, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_72, &ui_font_FontTaikongSize72, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_108, &ui_font_FontTaikongSize108, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_taikong_128, &ui_font_FontTaikongSize128, &lv_font_montserrat_48);
 
-    ui_font_set_fallback(&ui_font_FontBabyGearNumSize48, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontBabySize56, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontBabySize108, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontBabySize156, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontBabySize180, &lv_font_montserrat_48);
-    ui_font_set_fallback(&ui_font_FontBabySize200, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_baby_gear_48, &ui_font_FontBabyGearNumSize48, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_baby_56, &ui_font_FontBabySize56, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_baby_108, &ui_font_FontBabySize108, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_baby_156, &ui_font_FontBabySize156, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_baby_180, &ui_font_FontBabySize180, &lv_font_montserrat_48);
+    ui_font_set_fallback(&s_baby_200, &ui_font_FontBabySize200, &lv_font_montserrat_48);
 }
 
 static int16_t ui_font_typoder_size_runtime(int16_t base_px)
@@ -56,22 +79,22 @@ const lv_font_t *ui_font_typoder(int16_t base_px)
 {
     switch (ui_font_typoder_size_runtime(base_px)) {
     case 16:
-        return &ui_font_FontTypoderSize16;
+        return &s_typoder_16;
     case 20:
-        return &ui_font_FontTypoderSize20;
+        return &s_typoder_20;
     case 24:
-        return &ui_font_FontTypoderSize24;
+        return &s_typoder_24;
     case 28:
-        return &ui_font_FontTypoderSize28;
+        return &s_typoder_28;
     case 32:
-        return &ui_font_FontTypoderSize32;
+        return &s_typoder_32;
     case 36:
-        return &ui_font_FontTypoderSize36;
+        return &s_typoder_36;
     case 44:
-        return &ui_font_FontTypoderSize44;
+        return &s_typoder_44;
     case 56:
     default:
-        return &ui_font_FontTypoderSize56;
+        return &s_typoder_56;
     }
 }
 
